02cb8932310598de71c777631e7d984a33fe9399
Mell Lessington <melllessington@MacBook-Pro.local>
2020-07-22 17:12:58 -0400

    set up test bootstrap

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Container, Card, Button, NavProps, Navbar} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  
+  render() {
+    return (
+      <body>
+         
+
+        
+          <Container>
+          <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+        </Container>
+
+        <button onclick="myFunction()">Toggle dark mode</button>
+      </body>
+      
+    )
+  }
 }
-
-export default App;
